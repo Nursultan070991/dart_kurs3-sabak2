46559be56ed83580ba82de2b6611209e5e9060fc
MacBook <macbook@MacBook-Air-MacBook.local>
2024-01-16 19:58:42 +0300

    first commit

---
+++ bin/dart_kurs3_sabak2.dart
@@ -0,0 +1,79 @@
+
+
+ void main() {
+ print(1.runtimeType); 
+print(7.8.runtimeType);
+print('kanday'.runtimeType);
+print(true.runtimeType);
+
+  int a =1991;
+  int q =1998;
+  int w =1996;
+  int e =2018;
+  int r =2019;
+print(a);
+print(q);
+print(w);
+print(e);
+print(r);
+  
+  double t =10.12;
+  double y =20.5;
+  double u =30.12;
+  double i =4.14;
+  double o =7.9;
+print(t);
+print(y);
+print(u);
+print(i);
+print(o);
+
+String p ='kurs3';
+String s ='Nursultan';
+String d ='Mirsaly';
+String f ='Gulzada';
+String g ='Bakyt';
+print(p);
+print(s);
+print(d);
+print(f);
+print(g);
+
+void  kyrgyzstan(){}
+'osh';
+'chui';
+'naryn';
+'batken';
+'jalalabad';
+print('osh');
+print('chui');
+print('naryn');
+print('batken');
+print('jalalabad');
+
+dynamic a1 ='salam';
+dynamic a2 =1991;
+dynamic a3 =19.91;
+dynamic a4 ='kandai';
+dynamic a5 ='jakshy';
+print(a1);
+print(a2);
+print(a3);
+print(a4);
+print(a5);
+
+var b1 =18;
+var b2 =19;
+var b3 =20;
+var b4 =21;
+var b5 =22;
+print(b1);
+print(b2);
+print(b3);
+print(b4);
+print(b5);
+
+
+
+
+}
